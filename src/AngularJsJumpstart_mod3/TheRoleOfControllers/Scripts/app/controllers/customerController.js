70041a35cddf438867916615fdf276973579f203
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2016-01-13 13:11:25 -0600

    added a customerfactory singleton to be used in controllers.

--- src/AngularJsJumpstart_mod3/TheRoleOfControllers/Scripts/app/controllers/customerController.js
+++ src/AngularJsJumpstart_mod3/TheRoleOfControllers/Scripts/app/controllers/customerController.js
@@ -6,75 +6,14 @@
 (function () {
 
     // option that is recommended. create a variable that is your controller.
-    function CustomerController($scope) {
+    function CustomerController($scope, customersFactory) {
 
 
         $scope.sortBy = 'name';
         $scope.reverse = false;
 
-        $scope.customers = [
-            {
-                id: 1,
-                joined: '2000-12-02',
-                name: 'John',
-                city: 'Chandler',
-                orderTotal: 9.9956,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'Shoes',
-                        total: 9.9956
-                    }
-                ]
-            },
-            {
-                id: 2,
-                joined: '1965-01-25',
-                name: 'Zed',
-                city: 'Las Vegas',
-                orderTotal: 19.99,
-                orders: [
-                    {
-                        id: 2,
-                        product: 'Baseball',
-                        total: 9.995
-                    },
-                    {
-                        id: 3,
-                        product: 'Bat',
-                        total: 9.995
-                    }
-                ]
-            },
-            {
-                id: 3,
-                joined: '1944-06-15',
-                name: 'Tina',
-                city: 'New York',
-                orderTotal: 44.99,
-                orders: [
-                    {
-                        id: 4,
-                        product: 'Headphones',
-                        total: 44.99
-                    }
-                ]
-            },
-            {
-                id: 4,
-                joined: '1995-03-28',
-                name: 'Dave',
-                city: 'Seattle',
-                orderTotal: 101.50,
-                orders: [
-                    {
-                        id: 5,
-                        product: 'Kindle',
-                        total: 101.50
-                    }
-                ]
-            }
-        ];
+        $scope.customers = [];
+
 
         $scope.orderByMe = function (propName) {
 
@@ -82,10 +21,18 @@
             $scope.reverse = !$scope.reverse;
 
         }
+
+        function init() {
+
+            $scope.customers = customersFactory.getCustomers();
+
+        }
+
+        init();
     };
 
     // to ensure $scope parm isn't minified, inject it magically.
-    CustomerController.$inject = ['$scope'];
+    CustomerController.$inject = ['$scope', 'customersFactory'];
 
     myApp.controller('CustomerController', CustomerController);
 
--- src/AngularJsJumpstart_mod3/TheRoleOfControllers/Scripts/app/controllers/ordersController.js
+++ src/AngularJsJumpstart_mod3/TheRoleOfControllers/Scripts/app/controllers/ordersController.js
@@ -1,97 +1,25 @@
 ﻿(function () {
 
     // option that is recommended. create a variable that is your controller.
-    function OrdersController($scope, $routeParams) {
+    function OrdersController($scope, $routeParams, customersFactory) {
 
         // create a private var to track the routeParam in the route
         var customerId = $routeParams.customerId;
 
-        // we are going to bind to some orders for the view to use.
-        $scope.orders = null;
+        $scope.customer = null;
 
         // a private function to pre-get all orders for a particular customer
         function init() {
             // search the customers for the customerId
-            for (var i = 0, len = $scope.customers.length;
-                i < len; i++) {
-                if ($scope.customers[i].id === parseInt(customerId)) {
-                    // found the correct customer!
-                    $scope.orders = $scope.customers[i].orders;
-                    break;
-                }
-            }
+            $scope.customer = customersFactory.getCustomer(customerId);
         }
 
-
-        $scope.customers = [
-            {
-                id: 1,
-                joined: '2000-12-02',
-                name: 'John',
-                city: 'Chandler',
-                orderTotal: 9.9956,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'Shoes',
-                        total: 9.9956
-                    }
-                ]
-            },
-            {
-                id: 2,
-                joined: '1965-01-25',
-                name: 'Zed',
-                city: 'Las Vegas',
-                orderTotal: 19.99,
-                orders: [
-                    {
-                        id: 2,
-                        product: 'Baseball',
-                        total: 9.995
-                    },
-                    {
-                        id: 3,
-                        product: 'Bat',
-                        total: 9.995
-                    }
-                ]
-            },
-            {
-                id: 3,
-                joined: '1944-06-15',
-                name: 'Tina',
-                city: 'New York',
-                orderTotal: 44.99,
-                orders: [
-                    {
-                        id: 4,
-                        product: 'Headphones',
-                        total: 44.99
-                    }
-                ]
-            },
-            {
-                id: 4,
-                joined: '1995-03-28',
-                name: 'Dave',
-                city: 'Seattle',
-                orderTotal: 101.50,
-                orders: [
-                    {
-                        id: 5,
-                        product: 'Kindle',
-                        total: 101.50
-                    }
-                ]
-            }
-        ];
 
         init();
     };
 
     // to ensure $scope parm isn't minified, inject it magically.
-    OrdersController.$inject = ['$scope', '$routeParams'];
+    OrdersController.$inject = ['$scope', '$routeParams', 'customersFactory'];
 
     myApp.controller('OrdersController', OrdersController);
 
